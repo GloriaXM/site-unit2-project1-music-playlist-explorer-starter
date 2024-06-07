bacdbf7d01bbe825ce98272a43c74e0c85c66656
Gloria Ines Xiang-Yun Moudou <gloriamoudou@meta.com>
2024-06-06 21:11:47 -0700

    functional shuffle

--- music-playlist-creator/script.js
+++ music-playlist-creator/script.js
@@ -4,13 +4,13 @@ function generateCard(card){
     const newCard = document.createElement('button');
     newCard.className = 'card';
     const boundOpenModal = openModal.bind(this, card);
-    newCard.addEventListener('click', boundOpenModal)
+    newCard.addEventListener('click', boundOpenModal);
     newCard.innerHTML = `
         <img class="playlist-thumbnail" src="${card.playlist_art}">
         <h2> ${card.playlist_name}</h2>
         <p> ${card.playlist_creator}</p>
         <div class="like-bar">
-            <button>Here</button>
+            <img class="like-icon" src="./assets/img/favicon.ico">
             <p>0</p>
         </div>
     `;
@@ -23,46 +23,40 @@ function loadCards(){
         const newCard = generateCard(card);
         cardDeck.appendChild(newCard);
     })
+    let likes = document.getElementsByClassName("like-icon")
+
+    for (const single of likes){
+        single.addEventListener("click", function(event) {
+            let likeCount = event.target.parentNode.querySelector('p').textContent;
+            ++likeCount;
+            event.target.parentNode.querySelector('p').textContent = likeCount;
+            event.target.parentNode.querySelector('p').style.color = "#c30000";
+            event.stopPropagation();
+        });
+    }
+
 }
 
 function openModal(card){
-    const newModal = document.createElement('div');
-    newModal.className = 'modal-overlay';
+    const newModal = document.getElementById('modal-overlay');
     newModal.innerHTML = `
-
-        <div id="unique-view' class="modal-content">
-            <button id="close-button" class="close" onclick="this.parentNode.parentNode.remove()">x</button>  <!-- Code taken from https://stackoverflow.com/questions/19704477/adding-close-button-in-div-to-close-the-box -->
-                <img id="single-thumbnail" class="playlist-thumbnail" src="${card.playlist_art}">
-                <div>
-                    <h2>${card.playlist_name}</h2>
-                    <h3> Created by ${card.playlist_creator}</h3>
-                    <h3 class="shuffle-button"> Shuffle </h3>
-                    <div id="song-list">
-                    </div>
-
-                </div>
-
+    <div id="unique-view" class="modal-content">
+        <button id="close-button" class="close" onclick="this.parentNode.parentNode.style.display='none';">x</button>
+        <img id="single-thumbnail" class="playlist-thumbnail" src="${card.playlist_art}">
+        <div>
+            <h2>${card.playlist_name}</h2>
+            <h3> Created by ${card.playlist_creator}</h3>
+            <h3 id="shuffle-button"> Shuffle </h3>
+            <div id="song-list">
+            </div>
         </div>
+    </div>
     `;
 
-    document.body.appendChild(newModal);
-
-    // Load in songs
+    // // Load in songs
     const songList = document.getElementById('song-list');
-    //Loop using foreach
-    // card.songs.forEach(song => {
-    //     const songCard = document.createElement('div');
-    //     songCard.className = 'song-card';
-    //     songCard.innerHTML = `
-    //         <img class="cover-art" src= ${song.cover_art}>
-    //         <p> ${song.artist}</p>
-    //         <p> ${song.album}</p>
-    //         <p> ${song.duration}</p>
-    //     `;
-    //     songList.appendChild(songCard);
-    // })
 
-    //Load using index
+    // //Load using index
     for (let i = 0; i < card.songs.length; i++) {
         const song = card.songs[i];
         const songCard = document.createElement('div');
@@ -76,8 +70,51 @@ function openModal(card){
         songList.appendChild(songCard);
     }
 
+    const shuffleButton = document.getElementById('shuffle-button');
+    const boundShuffle = shuffle.bind(this, card.songs);
+    shuffleButton.addEventListener('click', boundShuffle);
 
+    // for (const song of shuffledArray){
+    //     console.log(song);
+    // }
+
+    newModal.style.display = 'block';
 }
+
+//shuffle algo taken from https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
+function shuffle(array) {
+    console.log("HERE");
+    let currentIndex = array.length;
+
+    // While there remain elements to shuffle...
+    while (currentIndex != 0) {
+
+      // Pick a remaining element...
+      let randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+
+      // And swap it with the current element.
+      [array[currentIndex], array[randomIndex]] = [
+        array[randomIndex], array[currentIndex]];
+    }
+
+    const songList = document.getElementById('song-list');
+    songList.innerHTML='';
+    for (let i = 0; i < array.length; i++) {
+        const song = array[i];
+        const songCard = document.createElement('div');
+        songCard.className = 'song-card';
+        songCard.innerHTML = `
+             <img class="cover-art" src= ${song.cover_art}>
+                <p> ${song.artist}</p>
+                <p> ${song.album}</p>
+                <p> ${song.duration}</p>
+        `;
+        songList.appendChild(songCard);
+    }
+
+    return array;
+  }
 
 
 loadCards();
